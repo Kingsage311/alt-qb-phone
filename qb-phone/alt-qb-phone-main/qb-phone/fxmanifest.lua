9038187131646e0b54e62a598b8214d64feb0ba8
Kings <84582203+Kingsage311@users.noreply.github.com>
2021-07-18 22:23:25 -0700

    Fixed fxmanifest so phone closes
    thanks to ihyajb#7177 for helping me out

---
+++ qb-phone/alt-qb-phone-main/qb-phone/fxmanifest.lua
@@ -0,0 +1,42 @@
+fx_version 'adamant'
+game 'gta5'
+
+description 'QB-Phone'
+version '1.0.0'
+
+ui_page 'html/index.html'
+
+client_scripts {
+    'client/main.lua',
+    'client/animation.lua',
+    'client/photo.lua',
+    'client/gui.lua',
+    'client/rentel.lua',
+}
+
+shared_scripts {
+    'config.lua',
+	'@qb-core/import.lua',
+    '@qb-apartments/config.lua',
+    '@qb-garages/SharedConfig.lua',
+}
+
+server_scripts {
+    'server/main.lua'
+}
+
+files {
+    'html/*.html',
+    'html/js/*.js',
+    'html/img/*.png',
+    'html/css/*.css',
+    'html/fonts/*.ttf',
+    'html/fonts/*.otf',
+    'html/fonts/*.woff',
+    'html/img/backgrounds/*.png',
+    'html/img/apps/*.png',
+}
+
+exports {
+    'InPhone'
+}
