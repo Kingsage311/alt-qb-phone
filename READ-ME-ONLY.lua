c621cb3a16490d99f3be1a7be51ea18d021aedca
Kings <84582203+Kingsage311@users.noreply.github.com>
2021-07-19 23:53:21 -0700

    added instruction's
    READ THE README

--- READ-ME-ONLY.lua
+++ READ-ME-ONLY.lua
@@ -1,3 +1,14 @@
+--[[This King's fork of Alt-QB-Phone if you are still useing a version of QBCore that has a "steam API" key in its server config please use buzzee's main branch 
+That can be found at https://github.com/MisterBuzzee/alt-qb-phone
+Now follow these step my step instructions:
+1:Rename or delete the old qb-phone and put the folder in this get called "qb-phone" in the recourse folder of your server files
+ DO NOT JUST PASTE IT OVER CURRENT QB-PHONE WILL CAUSE ISSUES
+2:If you plan on using dark sell add mg-pawnshop to your server recourse and ensure it your server.cfg
+3:Follow instructions below to add the fine
+
+If you are using my version of Alt-QB-Phone please don't ask or ping buzzee to help you with it this is a old phone he posted that I just updated to the new core
+and as my git profile says im a guy who has no idea what he's doing, so I don't want him dealing with issues i probably caused]]
+
 --==## Fines-for-phone ##==--
 
 -- Add this to [police/client/interactions.lua]
