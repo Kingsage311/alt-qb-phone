90b29e8dc9e13f80489eb481de3242b14b403ad9
Kings <84582203+Kingsage311@users.noreply.github.com>
2021-07-19 23:54:33 -0700

    typo

--- READ-ME-ONLY.lua
+++ READ-ME-ONLY.lua
@@ -4,7 +4,7 @@ Now follow these step my step instructions:
 1:Rename or delete the old qb-phone and put the folder in this get called "qb-phone" in the recourse folder of your server files
  DO NOT JUST PASTE IT OVER CURRENT QB-PHONE WILL CAUSE ISSUES
 2:If you plan on using dark sell add mg-pawnshop to your server recourse and ensure it your server.cfg
-3:Follow instructions below to add the fine
+3:Follow instructions below to add the fine system
 
 If you are using my version of Alt-QB-Phone please don't ask or ping buzzee to help you with it this is a old phone he posted that I just updated to the new core
 and as my git profile says im a guy who has no idea what he's doing, so I don't want him dealing with issues i probably caused]]
